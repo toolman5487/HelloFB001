022c1aafb253a1ae2c02309a3dd5e344d878f6b5
Willy <willy548798@gmail.com>
2024-12-13 13:09:07 +0800

    Firebase Storage v1

--- HelloFB001/ViewController.swift
+++ HelloFB001/ViewController.swift
@@ -6,14 +6,93 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseStorage
+import SDWebImage
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var theImageView: UIImageView!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    let auth = Auth.auth()
+    let ref = Storage.storage().reference()
+    var imageViews:[UIImageView] = []
+    var currentIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        reLoadPicture()
+        
+        //        Auth.auth().signInAnonymously { results, error in
+        //            if let error = error{
+        //                print(error.localizedDescription)
+        //            }else{
+        //                print(results?.user.uid)
+        //            }
+        //        }
+        auth.addStateDidChangeListener { auth, user in
+            if user == nil{
+                self.signInButton.setTitle("Sign In", for: .normal)
+            }else{
+                self.signInButton.setTitle("Sign Out", for: .normal)
+                print("Signed In:\(user!.uid)")
+            }
+        }
+        
     }
+    
+    func reLoadPicture(){
+        ref.child("test001").listAll { result, error in
+            if let error = error {
+                print("ERRORXXX \(error.localizedDescription)")
+                return
+            }
+            
+            self.imageViews.removeAll()
+            
+            if let items = result?.items{
+                for item in items {
+                    item.downloadURL { url, error in
+                        let imageView = UIImageView()
+                        imageView.sd_setImage(with: url) { _, _, _, _ in
+                            <#code#>
+                        }
+                    }
+                }
+            }
+            
+//            print("Name: \(result?.items[0].name)")
+//            if let ref = result?.items[0]{
+//                ref.downloadURL { url, error in
+//                    self.theImageView.sd_setImage(with: url)
+//                }
+//            }
+            
+        }
+    }
+    
+    @IBAction func nextPic(_ sender: Any) {
+        
+    }
+    
 
-
+    @IBAction func signInOut(_ sender: Any) {
+        if auth.currentUser == nil{
+            auth.signInAnonymously()
+        }else{
+            auth.currentUser?.delete(completion: { error in
+                print(error?.localizedDescription)
+            })
+            //            do{
+            //                try auth.signOut()
+            //            }catch{
+            //                print(error.localizedDescription)
+            //            }
+            //            signInButton.setTitle("Sign Out", for: .normal)
+            //            print("Signed In:\(auth.currentUser?.uid)")
+        }
+    }
+    
 }
 
